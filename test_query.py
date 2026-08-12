7ad049b08267ffe058b0e2aa960dda446ec52977
Laali <laalinembot@gmail.com>
2026-08-12 11:43:22 -0400

    Added row onto Eras table for testing purposes

--- test_query.py
+++ test_query.py
@@ -1,12 +1,14 @@
 from app import app
 from models.artists import Artists
+from models.eras import Eras
 from models.extensions import db
-
-db.init_app(app)
 
 with app.app_context():
     Artist1 = Artists(name="Playboi Carti", user_id="lnembot14")
-
     db.session.add(Artist1)
+    db.session.flush()
 
+    Era1 = Eras(name="WLRV1", user_id="lnembot14", artist_id= Artist1.id)
+    db.session.add(Era1)
     db.session.commit()
+
