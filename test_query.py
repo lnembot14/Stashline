677705e493e6da25735da40210a7574dac7c15ce
Laali <laalinembot@gmail.com>
2026-08-13 10:11:53 -0400

    Ran query on eras table

--- test_query.py
+++ test_query.py
@@ -1,6 +1,7 @@
 from app import app
 from models.artists import Artists
 from models.eras import Eras
+from models.tracks import Tracks
 from models.extensions import db
 
 with app.app_context():
@@ -10,5 +11,9 @@ with app.app_context():
 
     Era1 = Eras(name="WLRV1", user_id="lnembot14", artist_id= Artist1.id)
     db.session.add(Era1)
+    db.session.flush()
+
+    Track1 = Tracks(title="Skeleton", user_id = "lnembot", era_id = Era1.id, notes="Nice beat and mellow song")
+    db.session.add(Track1)
     db.session.commit()
 
